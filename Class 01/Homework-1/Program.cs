715ad8d8357d132f78562226e252fa4db9f1c152
bogoevski-s <slave.bogoevski@yahoo.com>
2021-03-26 23:31:36 +0100

    Homework init

---
+++ Class 01/Homework-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("     *");
+            Console.WriteLine("    ***");
+            Console.WriteLine("   *****");
+            Console.WriteLine("  *******");
+            Console.WriteLine(" *********");
+            Console.WriteLine("***********\n\n");
+
+            Console.WriteLine("    .***.");
+            Console.WriteLine("   *.   .*");
+            Console.WriteLine("     ***");
+            Console.WriteLine("      *");
+            Console.WriteLine("    * * *");
+            Console.WriteLine("  *   *   *");
+            Console.WriteLine("*     *     *");
+            Console.WriteLine("      *    ");
+            Console.WriteLine("     * *    ");
+            Console.WriteLine("    *   *    ");
+            Console.WriteLine("  *       *    ");
+        }
+    }
+}
---
+++ Class 01/Homework-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n\n");
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("\nHello " + name);
+            Console.ReadLine();
+        }
+    }
+}
