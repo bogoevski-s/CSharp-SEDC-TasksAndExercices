e953dde1fb29e39d3b4040f93635f8042c1f1a0e
bogoevski-s <slave.bogoevski@yahoo.com>
2021-03-27 21:29:49 +0100

    Class 02 - extra task added

---
+++ Class 02 - extra task/Days converter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Days_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Days Converter\n\n");
+            Console.WriteLine("Enter number of days to convert:");
+            string daysInput = Console.ReadLine();
+            bool dayParsed = int.TryParse(daysInput, out int days);
+
+            if (dayParsed)
+            {
+                int year = days / 365;
+                int week = (days % 365) / 7;
+                days = days - ((year * 365) + (week * 7));
+
+                if(year == week && year == days)
+                {
+                    Console.WriteLine("\nYou've entered a special number of days!");
+                }
+                Console.WriteLine("\n\n");
+                Console.WriteLine("Years: " + year);
+                Console.WriteLine("\nWeeks: " + week);
+                Console.WriteLine("\nDays: " + days);
+            }
+            else
+            {
+                Console.WriteLine("Wrong input!!!");
+            }
+        }
+    }
+}
---
+++ Class 02 - extra task/Task-01/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Largest number\n\n");
+            Console.WriteLine("Enter 4 numbers\n");
+            string numberOne = Console.ReadLine();
+            string numberTwo = Console.ReadLine();
+            string numberThree = Console.ReadLine();
+            string numberFour = Console.ReadLine();
+
+            bool firstNumParsed = int.TryParse(numberOne, out int numOne);
+            bool secondNumParsed = int.TryParse(numberTwo, out int numTwo);
+            bool thirdNumParsed = int.TryParse(numberThree, out int numThree);
+            bool fourthNumParsed = int.TryParse(numberFour, out int numFour);
+
+            if (firstNumParsed && secondNumParsed && thirdNumParsed && fourthNumParsed)
+            {
+                int[] numArr = new int[4];
+                numArr[0] = numOne;
+                numArr[1] = numTwo;
+                numArr[2] = numThree;
+                numArr[3] = numFour;
+                Array.Sort(numArr);
+                Console.WriteLine("\n\n");
+                Console.WriteLine("Largest number is : " + numArr[3]);
+            }
+            else
+            {
+                Console.WriteLine("\nNot a valid number!!");
+            }
+
+
+            //Finding largest number with loop 
+
+            //int[] numArr = new int[4];
+            //numArr[0] = numOne;
+            //numArr[1] = numTwo;
+            //numArr[2] = numThree;
+            //numArr[3] = numFour;
+
+            //int max = Int32.MinValue;
+
+            //for (int i = 0; i < numArr.Length; i++)
+            //{
+            //    if(numArr[i] > max)
+            //    {
+            //        max = numArr[i];
+            //    }
+            //}
+            //Console.WriteLine("\n" + max);
+
+        }
+    }
+}
