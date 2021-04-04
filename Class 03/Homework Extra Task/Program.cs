b5a8a5e03e8588c5cd316aa9f5d2a6f11c1fbaae
bogoevski-s <slave.bogoevski@yahoo.com>
2021-04-04 13:35:55 +0200

    Class 03 Bonus

---
+++ Class 03/Homework Extra Task/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Homework_Extra_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numberArray = new int[0];
+            int counter = 0;
+
+            while (true)
+            {
+                Console.WriteLine("\nEnter as many numbers as you like and press C to find the second smalesst number\nOr N to exit\n");
+
+                string userInput = Console.ReadLine();
+                bool userINputParsed = int.TryParse(userInput, out int inputNum);
+
+                if (userInput == "n" || userInput == "N")
+                {
+                    break;
+                }
+                else if (userInput == "c" || userInput == "C")
+                {
+                    Array.Sort(numberArray);
+                    Console.WriteLine("Second smallest number is " + numberArray[1]);
+                }
+                else if (!userINputParsed)
+                {
+                    Console.WriteLine("\nWrong input!!\n");
+                }
+                else
+                {
+                    counter++;
+                    Array.Resize(ref numberArray, counter);
+                    numberArray[counter - 1] = inputNum;
+                }
+            }
+        }
+    }
+}
