85e0f901c2444dcf1d4e960fb8352107e1718345
bogoevski-s <slave.bogoevski@yahoo.com>
2021-04-04 18:02:38 +0200

    Class 04 Homework

--- Class 04/Homework/Program.cs
+++ Class 04/Homework/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Homework
 {
@@ -6,7 +7,21 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Age Calculator");
+            Console.WriteLine($@"Enter your age in this format ""dddd MMM yyyy"" seperated by coma ");
+
+            string inputDate = Console.ReadLine();
+            DateTime date = DateTime.Parse(inputDate);
+
+            AgeCalculator(date);
+        }
+        static void AgeCalculator(DateTime date)
+        {
+            DateTime today = DateTime.Now;
+            int todayYear = today.Year;
+            int bornYear = date.Year;
+
+            Console.WriteLine($"\nYou are -> {todayYear - bornYear} years old");
         }
     }
 }
