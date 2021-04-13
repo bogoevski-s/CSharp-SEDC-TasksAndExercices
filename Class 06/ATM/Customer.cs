4ef9a797ad773d5f03fc0bc2a641346ee369dff0
bogoevski-s <slave.bogoevski@yahoo.com>
2021-04-13 19:46:47 +0200

    ATM homework fix

--- Class 06/ATM/Customer.cs
+++ Class 06/ATM/Customer.cs
@@ -9,14 +9,14 @@ namespace ATM
         public string FirstName { get; set; }
         public string LastName { get; set; }
         private int Pin { get; set; }
-        public int CardNumber { get; set; }
+        public long CardNumber { get; set; }
         private int Balance { get; set; }
 
         public Customer()
         {
 
         }
-        public Customer(string firstName, string lastName, int pin, int cardNumber)
+        public Customer(string firstName, string lastName, int pin, long cardNumber)
         {
             FirstName = firstName;
             LastName = lastName;
@@ -24,7 +24,7 @@ namespace ATM
             CardNumber = cardNumber;
         }
 
-        public Customer(string firstName, string lastName, int pin, int cardNumber, int balance)
+        public Customer(string firstName, string lastName, int pin, long cardNumber, int balance)
         {
             FirstName = firstName;
             LastName = lastName;
@@ -52,6 +52,22 @@ namespace ATM
         public int CheckBalance()
         {
             return Balance;
+        }
+        public int Cashwithdrawal(int cash)
+        {
+            if (Balance >= cash)
+            {
+                Balance -= cash;
+                return Balance;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public void CashDeposit(int amount)
+        {
+            Balance += amount;
         }
     }
 }
--- Class 06/ATM/Program.cs
+++ Class 06/ATM/Program.cs
@@ -6,39 +6,155 @@ namespace ATM
     {
         static void Main(string[] args)
         {
-            Customer newCustomer = new Customer("Slave", "Bogoevski", 1234, 11112222);
-            Customer customer2 = new Customer("Renata", "Bogoevska", 5555, 098765, 500);
 
-            Console.WriteLine(newCustomer.WellcomeMessage());
-            Console.WriteLine();
-            newCustomer.SetPin(9999);
-            Console.WriteLine(customer2.CheckBalance());
-            customer2.SetBalance(300);
-            Console.WriteLine(customer2.CheckBalance());
+            Customer[] customers = new Customer[]
+            {
+                new Customer("Slave", "Bogoevski", 1234, 4111222233334444, 1500),
+                new Customer("Renata", "Bogoevska", 5555, 1111222233334444, 500)
+            };
 
-            if (5555 == customer2.CheckPin()){
-                Console.WriteLine($"Customer pin: {customer2.CheckPin()}");
+            //newCustomer.SetPin(9999);
+            //Console.WriteLine(customer2.CheckBalance());
+
+            //Console.WriteLine("Enter amount to withdrawal:");
+
+            //string cashWithdrawal = Console.ReadLine();
+            //bool cashWithdrawlParsed = int.TryParse(cashWithdrawal, out int withdrawalNum);
+
+            //customer2.Cashwithdrawal(withdrawalNum);
+
+            //if (customer2.Cashwithdrawal(withdrawalNum) == -1)
+            //{
+            //    Console.WriteLine("Not enough money");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("After cashwithdraw");
+            //    Console.WriteLine($"{customer2.CheckBalance()}$");
+            //}
+
+            //Console.WriteLine("Enter amount to deposit:");
+
+            //string deposit = Console.ReadLine();
+            //bool depositParsed = int.TryParse(deposit, out int depositInt);
+            //customer2.CashDeposit(depositInt);
+            //Console.WriteLine("After deposit");
+            //Console.WriteLine($"{customer2.CheckBalance()}$");
+
+                Console.WriteLine($@"Enter you card number ""XXXX-XXXX-XXXX-XXXX"":");
+                string cardNumber = Console.ReadLine();
+                string cardNumberFinal = "";
+
+                Console.WriteLine("Enter pin");
+                string userPinInput = Console.ReadLine();
+                bool pinParsed = int.TryParse(userPinInput, out int pinNumber);
+
+                if (cardNumber.Contains("-"))
+                {
+                    cardNumberFinal = cardNumber.Replace("-", "").Trim();
+                }
+                else if (cardNumber.Contains(" "))
+                {
+                    cardNumberFinal = cardNumber.Replace(" ", "").Trim();
+                }
+                else
+                {
+                    cardNumberFinal = cardNumber;
+                }
+
+                bool credtCardParsed = long.TryParse(cardNumberFinal, out long creditCardInt);
+
+                if (!credtCardParsed)
+                {
+                    Console.WriteLine("\nWrong input | Please use numbers\nEnter 16 digit number from your card\n");
+             
+                }
+                
+                if(CreditCardCheck(creditCardInt, pinNumber, customers))
+                {
+                    Console.WriteLine("1.Balance checking\n2.Cash withdrawal\n2.Cash deposition");
+                }
+                else
+                {
+                    Console.WriteLine("Please try again");
+                }
+
+                
+                
+            
+            
+
+
+
+
+            static bool CreditCardCheck(long cardNumber, int pin, Customer[] customers)
+            {
+                foreach(Customer customer in customers)
+                {
+                    if(cardNumber == customer.CardNumber)
+                    {
+                        if(PinCheck(pin, customer))
+                        {
+                            return true;
+                        }
+                    }
+                    
+                }
+                Console.WriteLine("Wrong Credit Card Number!!");
+                return false;
             }
 
-            Console.WriteLine($@"Enter you card number ""XXXX-XXXX-XXXX-XXXX"":");
-            string cardNumber = Console.ReadLine();
-            //bool cardNumParsed = long.TryParse(cardNumber, out long cardNumLong);
+            static bool PinCheck(int pin, Customer customer)
+            {
 
-            //long carNumber = 1111222233334444;
+                if (pin == customer.CheckPin())
+                {
 
-            //String.Format("{0:0000-0000-0000-0000}", cardNumber);
+                    Console.WriteLine($"Correct pin \u221A");
+                    Console.WriteLine(customer.WellcomeMessage());
+                    return true;
+                }
 
-            //string newNum = String.Format("{0:0000-0000-0000-0000}", cardNumLong);
-            //Console.WriteLine(newNum);
-
-            //Console.WriteLine($@"Enter you card number ""XXXX-XXXX-XXXX-XXXX"":");
-            //string cardNumberInput = Console.ReadLine().Replace(" ", "");
-
-            Regex reg = new Regex("\b4[0-9]{12}(?:[0-9]{3})?\b");
-            var isMatch = reg.IsMatch(cardNumber);
-            Console.WriteLine(isMatch);
+                Console.WriteLine("Wrong pin input!!");
+                return false;
+            }
 
 
+
+
+            static bool CreditCardCreate(string cardNumber, Customer[] customers)
+            {
+                string pat = "^4[0-9]{12}(?:[0-9]{3})?$";
+                string pat2 = @"\b\d{13,16}\b";
+
+                Regex r = new Regex(pat2, RegexOptions.IgnoreCase);
+
+                //string cardNumberFinal = "";
+
+                //foreach(Customer customer in customers)
+                //{
+                //    if (cardNumber.Contains("-"))
+                //    {
+                //        cardNumberFinal = cardNumber.Replace("-", "");
+                //        Console.WriteLine(r.IsMatch(cardNumberFinal));
+                //        return r.IsMatch(cardNumberFinal);
+                //    }
+                //    else if (cardNumber.Contains(" "))
+                //    {
+                //        cardNumberFinal = cardNumber.Replace(" ", "").Trim();
+                //        Console.WriteLine(r.IsMatch(cardNumberFinal));
+                //        return r.IsMatch(cardNumberFinal);
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine(r.IsMatch(cardNumber));
+                //        return r.IsMatch(cardNumberFinal);
+                //    }
+                //}
+                Console.WriteLine(r.IsMatch(cardNumber));
+                return r.IsMatch(cardNumber);
+
+            }
         }
     }
 }
