e5a7c335d5092720d94f40ced693e812cf950986
bogoevski-s <slave.bogoevski@yahoo.com>
2021-04-18 19:37:25 +0200

    Class 08 homework init

---
+++ Class 08/Homework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> intQueue = new Queue<int>();
+
+            Console.WriteLine("Please input number:");
+            string userInput = Console.ReadLine();
+            bool userInputParsed = int.TryParse(userInput, out int inputNumber);
+            if (!userInputParsed)
+            {
+                Console.WriteLine("You need to eneter numberic value");
+            }
+            else
+            {
+                intQueue.Enqueue(inputNumber);
+            }
+
+
+        }
+    }
+}
