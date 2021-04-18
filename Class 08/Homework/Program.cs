4d6ced8a4175e932ea297851dd5a61423eee53bc
bogoevski-s <slave.bogoevski@yahoo.com>
2021-04-18 19:52:04 +0200

    fix

--- Class 08/Homework/Program.cs
+++ Class 08/Homework/Program.cs
@@ -8,17 +8,49 @@ namespace Homework
         {
             Queue<int> intQueue = new Queue<int>();
 
-            Console.WriteLine("Please input number:");
-            string userInput = Console.ReadLine();
-            bool userInputParsed = int.TryParse(userInput, out int inputNumber);
-            if (!userInputParsed)
+            while (true)
             {
-                Console.WriteLine("You need to eneter numberic value");
+                Console.WriteLine("Please input number:\n");
+                string userInput = Console.ReadLine();
+                bool userInputParsed = int.TryParse(userInput, out int inputNumber);
+                if (!userInputParsed)
+                {
+                    Console.WriteLine("\nYou need to eneter numberic value\n");
+
+                    continue;
+                }
+                else
+                {
+                    intQueue.Enqueue(inputNumber);
+
+                    while (true)
+                    {
+                        Console.WriteLine("\n1.Add another number\n2.Exit\n");
+                        string userAddOrExit = Console.ReadLine();
+                        bool parsedUserOption = int.TryParse(userAddOrExit, out int menuNum);
+                        if (!parsedUserOption || menuNum < 1 || menuNum > 2)
+                        {
+                            Console.WriteLine("\nWrong input!!\nPlease select 1 or 2");
+                            continue;
+                        }
+                        else if(menuNum == 1)
+                        {
+                            break;
+                        }
+                        else if(menuNum == 2)
+                        {
+                            Console.WriteLine("\nAll your numbers:\n");
+                            foreach(int num in intQueue)
+                            {
+                                Console.WriteLine(num);
+                            }
+                            return;
+                        }
+                    }
+                    
+                }
             }
-            else
-            {
-                intQueue.Enqueue(inputNumber);
-            }
+            
 
 
         }
